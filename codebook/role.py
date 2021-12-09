855e8142e24dbec3e171dcf81ece204be8537166
Jennas-Lee <leezeunjun@gmail.com>
2021-12-09 21:58:34 +0900

    Add codebook

---
+++ codebook/role.py
@@ -0,0 +1,11 @@
+ROLE_LIST = [
+    1,  # 보안관
+    2, 2,  # 부관
+    3, 3, 3,  # 무법자
+    4, 4  # 배신자
+]
+"""
+    역할 상수입니다.
+
+    <a href="https://github.com/Jennas-Lee/bang-kr-online-server/wiki/Role-Card-Codebook">위키 보기</a>
+"""
